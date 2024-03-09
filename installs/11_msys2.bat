63fcb7b0611e360bfe62f4b539f4f751419c1b15
Laurent Boulard <laurent.boulard@gmail.com>
2024-03-09 16:34:55 +0100

    installs: user install of msys2 in C:\msys64 (or C:\dev\msys64)

---
+++ installs/11_msys2.bat
@@ -0,0 +1,36 @@
+@SETLOCAL
+@CHCP 65001 >NUL:
+@CALL "%~dp0\00_config.bat"
+@IF ERRORLEVEL 1 GOTO :exit
+@CD /D "%ROOT_MSYS2%"
+@IF ERRORLEVEL 1 GOTO :exit
+
+@SET PRG=
+@FOR %%f IN ("msys2-x86_64-*.exe") DO @SET "PRG=%%~f"
+@ECHO SET PRG=%PRG%
+@IF NOT DEFINED PRG (
+ECHO ** ERROR: No installation program found
+SET ERRORLEVEL=64
+GOTO :exit
+)
+
+@SET DEST=C:/msys64
+@IF EXIST C:\DEV\. @SET DEST=C:/DEV/msys64
+
+@IF "%1"=="uninstall" @GOTO :uninstall
+
+".\%PRG%" in --confirm-command --accept-messages --root %DEST%
+
+@:: Pause if not interactive
+@:exit
+@SET ERR=%ERRORLEVEL%
+@IF DEFINED _ELEV GOTO :_elev
+@IF ERRORLEVEL 1 @ECHO Failure ERRORLEVEL=%ERRORLEVEL%
+@SET ERRORLEVEL=0
+@ECHO %cmdcmdline% | FIND /i "%~0" >NUL
+@IF NOT ERRORLEVEL 1 PAUSE
+@:_elev
+@ENDLOCAL&EXIT /B %ERR%
+
+@:uninstall
+%DEST:/=\%\uninstall.exe pr --confirm-command
